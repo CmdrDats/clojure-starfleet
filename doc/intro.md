053900136208ad00f1962224f2050cc47c421584
Deon Moolman <deon@yuppiechef.com>
2015-05-11 19:50:46 +0200

    Initial project creation - nothing much to see here yet!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-starfleet
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_starfleet/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-starfleet.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-starfleet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
